b58f9a76330bb874bfbb07826e9d6cc6eb6c3aa5
NishantP43 <nishant.patil0403@gmail.com>
2024-07-04 11:18:51 +0530

    modified:   .gitignore 	modified:   demo.go 	modified:   go.mod 	modified:   go.sum 	new file:   handler_readiness.go 	new file:   json.go 	new file:   vendor/github.com/go-chi/chi/.gitignore 	new file:   vendor/github.com/go-chi/chi/CHANGELOG.md	new file:   vendor/github.com/go-chi/chi/CONTRIBUTING.md 	new file:   vendor/github.com/go-chi/chi/LICENSE 	new file:   vendor/github.com/go-chi/chi/Makefile 	new file:   vendor/github.com/go-chi/chi/README.md 	new file:   vendor/github.com/go-chi/chi/chain.go 	new file:   vendor/github.com/go-chi/chi/chi.go 	new file:   vendor/github.com/go-chi/chi/context.go 	new file:   vendor/github.com/go-chi/chi/go.mod 	new file:   vendor/github.com/go-chi/chi/mux.go 	new file:   vendor/github.com/go-chi/chi/tree.go 	new file:   vendor/github.com/go-chi/cors/LICENSE 	new file:   vendor/github.com/go-chi/cors/README.md 	new file:   vendor/github.com/go-chi/cors/cors.go 	new file:   vendor/github.com/go-chi/cors/go.mod 	new file:   vendor/github.com/go-chi/cors/utils.go 	modified:   vendor/modules.txt

--- demo.go
+++ demo.go
@@ -3,19 +3,47 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("")
 
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("$PORT must be set")
 	}
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		ExposedHeaders:   []string{"Link"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Print("Server Starting on Port ", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("PORT: ", portString)
 
 }
